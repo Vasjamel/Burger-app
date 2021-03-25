53083efc8a434a7d74323b26e06815ebf56b645d
Vasyl <vasjanja@gmail.com>
2021-03-25 07:35:36 +0200

    adjustment-app

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,17 +1,36 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Hoc from '../../hoc/Hoc'
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 import Toolbar from '../Navigation/Toolbar/Toolbar'
 import classes from './../Layout/Layout.module.css'
 
-const Layout = (props) => {
-  return (
-    <Hoc>
-      <Toolbar />
-      <SideDrawer />
-      <main className={classes.content}>{props.children}</main>
-    </Hoc>
-  )
+class Layout extends Component {
+  state = {
+    showSideDrawer: false,
+  }
+
+  sideDrawerCloseHandler = () => {
+    this.setState({ showSideDrawer: false })
+  }
+
+  drawerToggleHandler = () => {
+    this.setState((oldState) => {
+      return { showSideDrawer: !oldState.showSideDrawer }
+    })
+  }
+
+  render() {
+    return (
+      <Hoc>
+        <Toolbar drawerToggleClicked={this.drawerToggleHandler} />
+        <SideDrawer
+          open={this.state.showSideDrawer}
+          closed={this.sideDrawerCloseHandler}
+        />
+        <main className={classes.content}>{this.props.children}</main>
+      </Hoc>
+    )
+  }
 }
 
 export default Layout
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,19 +2,27 @@ import React from 'react'
 import Logo from '../../Logo/Logo'
 import NavigationItems from '../NavigationItems/NavigationItems'
 import classes from './SideDrawer.module.css'
+import Backdrop from './../../UI/Backdrop/Backdrop'
+import Hoc from './../../../hoc/Hoc'
 
 const SideDrawer = (props) => {
-  //...
+  let attachClasses = [classes.SideDrawer, classes.Close]
+  if (props.open) {
+    attachClasses = [classes.SideDrawer, classes.Open]
+  }
 
   return (
-    <div className={classes.SideDrawer}>
-      <div className={classes.Logo}>
-        <Logo />
+    <Hoc>
+      <Backdrop show={props.open} hideModal={props.closed} />
+      <div className={attachClasses.join(' ')}>
+        <div className={classes.Logo}>
+          <Logo />
+        </div>
+        <nav>
+          <NavigationItems />
+        </nav>
       </div>
-      <nav>
-        <NavigationItems />
-      </nav>
-    </div>
+    </Hoc>
   )
 }
 
